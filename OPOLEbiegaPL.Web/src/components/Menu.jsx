5cc25095e3beafd34083b5b4ab355f0029e8e9eb
ReszkeM <marek.reszke09@gmail.com>
2016-08-17 14:37:10 +0200

    Add necessary libraries and create test views
    - bootstrap
    - font-awesome
    - jquery 3.1.0

---
+++ OPOLEbiegaPL.Web/src/components/Menu.jsx
@@ -0,0 +1,16 @@
+﻿import React from 'react';
+
+export default React.createClass({
+    getMenuItems: function() {
+        return this.props.menuItems || [];
+    },
+    render: function() {
+        return <div className="menu">
+          {this.getMenuItems().map(entry =>
+              <div key={entry}>
+              <h1>{entry}</h1>
+            </div>
+        )}
+      </div>;
+}
+});
---
+++ OPOLEbiegaPL.Web/src/index.jsx
@@ -0,0 +1,18 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+import Menu from './components/Menu';
+
+const menuItems = ['Persons', 'Friends', 'Events', 'Announcement', 'Contact'];
+
+// render menu
+ReactDOM.render(
+  <Menu menuItems={menuItems} />,
+  document.getElementById('menu')
+);
+
+// render site content
+ReactDOM.render(
+  <Menu menuItems={menuItems} />,
+  document.getElementById('content')
+);
+
