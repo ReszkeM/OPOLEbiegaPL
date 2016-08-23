5aef5ba02e265b5be1530d363a86f254625673b1
ReszkeM <marek.reszke09@gmail.com>
2016-08-23 13:52:33 +0200

    move get and send data logic to fetchHelper

--- OPOLEbiegaPL.Web/src/helpers/fetchHelpers.js
+++ OPOLEbiegaPL.Web/src/helpers/fetchHelpers.js
@@ -1,4 +1,6 @@
-﻿var fetchGET = function(url, callback) {
+﻿import {setState} from '../action_creators';
+
+var fetchGET = function (url, callback) {
     fetch(url, {
         method: 'GET',
         dataType: "json",
@@ -19,9 +21,53 @@ var fetchPOST = function(url, data, callback) {
     }).then((result) => callback(result));
 };
 
+function handlePOST(action, store) {
+    fetchPOST(action.meta.url, action.entry, function (result) {
+        console.log(result);
+        if (result.ok) {
+            switch (action.meta.propName) {
+                case 'Friends':
+                    store.dispatch(setState(store, ['Friends']));
+                case 'Events':
+                    store.dispatch(setState(store, ['Events']));
+                case 'Persons':
+                    store.dispatch(setState(store, ['Persons']));
+                case 'Announcements':
+                    store.dispatch(setState(store, ['Announcements']));
+            }
+        }
+    });
+};
+
+function handleGET(url, store, propName) {
+    fetchGET(url, function (result) {
+        store.dispatch({
+            type: 'SET_STATE',
+            state: { [propName]: result }
+        });
+
+        return result;
+    });
+};
+
+function getData(action, store) {
+    if (action.meta.propsToUpdate.indexOf('Friends') !== -1) {
+        handleGET('http://localhost:57174/api/FriendApi/GetAll', store, 'friends');
+    }
+    if (action.meta.propsToUpdate.indexOf('Events') !== -1) {
+        handleGET('http://localhost:57174/api/EventApi/GetAll', store, 'events');
+    }
+    if (action.meta.propsToUpdate.indexOf('Persons') !== -1) {
+        handleGET('http://localhost:57174/api/PersonApi/GetAll', store, 'persons');
+    }
+    if (action.meta.propsToUpdate.indexOf('Announcements') !== -1) {
+        handleGET('http://localhost:57174/api/AnnouncementApi/GetAll', store, 'announcements');
+    }
+}
+
 var fetchHelper = {
-    fetchGET: fetchGET,
-    fetchPOST: fetchPOST
+    getData: getData,
+    sendData: handlePOST
 };
 
 module.exports = fetchHelper;
--- OPOLEbiegaPL.Web/src/remote_action_middleware.js
+++ OPOLEbiegaPL.Web/src/remote_action_middleware.js
@@ -1,69 +1,12 @@
-﻿import {setState} from './action_creators';
-import * as fetchHelper from './helpers/fetchHelpers';
+﻿import * as fetchHelper from './helpers/fetchHelpers';
 
 export default store => next => action => {
     if (action.meta && action.meta.remote) {
         if (action.meta.method === 'POST') {
-            fetchHelper.fetchPOST(action.meta.url, action.entry, function(result) {
-                console.log(result);
-                if (result.ok) {
-                    switch(action.meta.propName) {
-                        case 'Friends':
-                            store.dispatch(setState(store, ['Friends']));
-                        case 'Events':
-                            store.dispatch(setState(store, ['Events']));
-                        case 'Persons':
-                            store.dispatch(setState(store, ['Persons']));
-                        case 'Announcements':
-                            store.dispatch(setState(store, ['Announcements']));
-                    }
-                }
-            });
+            fetchHelper.sendData(action, store);
         }
         if (action.meta.method === 'GET') {
-            if (action.meta.propsToUpdate.indexOf('Friends') !== -1)
-                fetchHelper.fetchGET('http://localhost:57174/api/FriendApi/GetAll', function(result) {
-                    store.dispatch({
-                        type: 'SET_STATE',
-                        state: {
-                            friends: result
-                        }
-                    });
-                    return result;
-                });
-
-            if (action.meta.propsToUpdate.indexOf('Events') !== -1)
-                fetchHelper.fetchGET('http://localhost:57174/api/EventApi/GetAll', function(result) {
-                    store.dispatch({
-                        type: 'SET_STATE',
-                        state: {
-                            events: result
-                        }
-                    });
-                    return result;
-                });
-
-            if (action.meta.propsToUpdate.indexOf('Persons') !== -1)
-                fetchHelper.fetchGET('http://localhost:57174/api/PersonApi/GetAll', function(result) {
-                    store.dispatch({
-                        type: 'SET_STATE',
-                        state: {
-                            persons: result
-                        }
-                    });
-                    return result;
-                });
-
-            if (action.meta.propsToUpdate.indexOf('Announcements') !== -1)
-                fetchHelper.fetchGET('http://localhost:57174/api/AnnouncementApi/GetAll', function(result) {
-                    store.dispatch({
-                        type: 'SET_STATE',
-                        state: {
-                            announcements: result
-                        }
-                    });
-                    return result;
-                });
+            fetchHelper.getData(action, store);
         }
     } 
     return next(action);
