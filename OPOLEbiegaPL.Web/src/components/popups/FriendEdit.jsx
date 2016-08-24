5a9bc72f003f9a9c5cd0cf20260422dabf2c944e
ReszkeM <marek.reszke09@gmail.com>
2016-08-24 23:21:32 +0200

    Create base modal window and one specyfic content

---
+++ OPOLEbiegaPL.Web/src/components/popups/FriendEdit.jsx
@@ -0,0 +1,20 @@
+﻿import React from 'react';
+
+export default React.createClass({
+  render: function () {
+      return <div>
+                  <div className="form-group row">
+                      <label className="col-md-3" style={this.props.styles.labelStyle}>Nazwa: </label>
+                      <div className="col-md-9">
+                          <input className="form-control" id="name" type="text" defaultValue={this.props.name} onChange={this.props.handleChange} />
+                      </div>
+                  </div>
+                  <div className="form-group row">
+                      <label className="col-md-3" style={this.props.styles.labelStyle}>Obrazek: </label>
+                      <div className="col-md-9">
+                          <input className="form-control" id="imageURL" type="text" defaultValue={this.props.imageURL} onChange={this.props.handleChange} />
+                      </div>
+                  </div>
+              </div>
+  }
+});
---
+++ OPOLEbiegaPL.Web/src/components/popups/Modal.jsx
@@ -0,0 +1,53 @@
+﻿import React from 'react';
+
+export default React.createClass({
+  getInitialState: function() {
+      return this.props.object;
+  },
+
+  handleChange: function(event) {
+      var returnObj;
+      var stateObject = function() {
+          returnObj = {};
+          returnObj[this.target.id] = this.target.value;
+          return returnObj;
+      }.bind(event)();
+      this.setState( stateObject );
+  },
+
+  render: function () {
+      var scope = {
+          modalStyles: {
+              display: 'block'
+          },
+          title: {
+            fontWeight: 'bold'
+          },
+          childStyles: {
+              labelStyle:{
+                  fontSize: '22px'
+              }
+          }
+      };
+
+      return <div>
+                  <div className="modal-backdrop in"></div>
+                  <div className="modal in"  role='dialog' style={scope.modalStyles}>
+                      <div className="modal-dialog">
+                          <div className="modal-content">
+                              <div className="modal-header">
+                                  <h2 className="modal-title" style={scope.title}>{this.props.title}</h2>
+                              </div>
+                              <div className="modal-body">
+                                  <this.props.component {...this.state} styles={scope.childStyles} handleChange={this.handleChange} />
+                              </div>
+                              <div className="modal-footer">
+                                  <button className="btn btn-default" onClick={() => this.props.hideWindow({isPopupVisible: false})}> Anuluj </button>
+                                  <button className="btn btn-primary" onClick={() => this.props.save(this.state)}> Zapisz </button>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+    }
+});
