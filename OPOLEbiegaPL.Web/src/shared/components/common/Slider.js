5c836fd147016c184a890cfa851b5062221dc4c8
Marek Reszke <marek.reszke@euvic.pl>
2016-09-09 13:26:25 +0200

    Common slider

---
+++ OPOLEbiegaPL.Web/src/shared/components/common/Slider.js
@@ -0,0 +1,24 @@
+﻿import React from 'react';
+import Carousel from 'nuka-carousel';
+
+// styles
+import {block} from '../../constants/styles'
+
+export const Slider = React.createClass({
+    mixins: [Carousel.ControllerMixin],
+
+    renderCarousel: function() {
+        if (this.props.collection.length !== 0){
+            return  <Carousel style={block} className="center-block">
+                        { this.props.collection.map( (object) =>
+                            <this.props.component key={object.Id} object={object} action={this.showDetailsButtonClick} />
+                        )}
+                    </Carousel>
+        }
+        return null;
+    },
+
+    render: function() {
+        return this.renderCarousel();
+    }
+});
